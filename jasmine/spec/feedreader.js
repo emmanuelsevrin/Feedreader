6bd90743b497523c9d2c38642aa3c4088c443cd7
Emmanuel Sevrin <emmanuel.sevrin@gmail.com>
2018-03-01 22:49:21 +0200

    Update comments in feedreader

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,34 +4,19 @@
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+
 $(function() {
 
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
 
     describe('RSS Feeds', function() {
         
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-
+        // tests that the allFeeds variable has been defined
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         })
  
-        //function testing that a specific feed has a defined, non-null URL 
-	        
+        //function testing that a specific feed has a defined, non-null URL       
         function test_URLs(feed){
             it('the \'' + feed.name + '\' feed has an URL', function(){
                 expect(feed.url).toBeDefined();
@@ -39,6 +24,7 @@ $(function() {
             })
         }
 
+        //function testing that a specific feed has a name
          function test_name(feed, index){
             it('the ' + index + '(st) feed has a name', function(){
                 expect(feed.name).toBeDefined();
@@ -51,7 +37,6 @@ $(function() {
             test_URLs(feed);
          };
 
-         //function testing that a specific feed at a specific index position has a defined, non-null name
 
          //looping through allfeeds to make sure they all have a name
         for(let i=0; i < allFeeds.length; i++){
@@ -62,10 +47,12 @@ $(function() {
 
     describe('The Menu', function(){
 
+        //making sure the menu is hidden by default
         it('is hidden by default', function(){
             expect($("body").hasClass("menu-hidden")).toBe(true);
         })
         
+        //making sure the menu has visibility when clicked
         it('changes visibility when clicked', function(){
             $(".menu-icon-link").click();
             expect($('body').hasClass("menu-hidden")).not.toBe(true);
